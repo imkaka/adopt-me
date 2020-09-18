26bdaa4a0da905e8c1f70a0e675a2e09c8b7eaa0
Anil Khatri <anil.soccer.khatri@gmail.com>
2020-09-18 09:28:02 +0530

    :tada: Adopt me with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { unmountComponentAtNode } from "react-dom";
 
 function App() {
+  const breeds = ["boxer", "pug", "akita", "pitbull"];
+
+  const [breed, setBreed] = useState(breeds[0]);
+  const [imageUrl, setImageUrl] = useState(
+    "https://images.dog.ceo/breeds/boxer/n02108089_2723.jpg"
+  );
+
+  // componentDidMount, Update
+  useEffect(() => {
+    const dogURL = `https://dog.ceo/api/breed/${breed}/images/random`;
+
+    fetch(dogURL)
+      .then((res) => res.json())
+      .then((res) => setImageUrl(res.message));
+  }, [breed]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Adopt Me</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="breed">Select Breed</label>
+          <select
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            className="form-control"
+            id="breed"
+          >
+            {breeds.map((breed, index) => (
+              <option key={index}>{breed}</option>
+            ))}
+          </select>
+        </div>
+      </form>
+      <img src={imageUrl} className="rounded img-fluid" />
     </div>
   );
 }
 
 export default App;
+
+
+
+app(z) > counters -> counter -> x -> y(z) 
